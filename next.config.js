c24c97ede6cc466bb320c94b1f0ccc55a2749600
mahdiJ2001 <mahdio.jellali@hotmail.fr>
2025-11-26 10:44:30 +0100

    Remove conflicting next.config.ts file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  trailingSlash: false
+    output: 'standalone',
+    trailingSlash: false
 };
 
 module.exports = nextConfig;
